a20d5338330fe02ba20b9ce3dcc1757f0f3a09ec
Tazro Inutano Ohta <inutano@gmail.com>
2015-09-03 00:18:58 +0900

    tests for a valid url, saved image

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,18 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'thunderbuddies'
+require 'net/http'
+
+def url_exist?(url_string)
+  url = URI.parse(url_string)
+  req = Net::HTTP.new(utl.host, url.port)
+  req.use_ssl = (url.scheme == 'https')
+  path = url.path if url.path.present?
+  res = req.request_head(path || '/')
+  if res.kind_of?(Net::HTTPRedirection)
+    url_exist?(res['location'])
+  else
+    res.code[0] != "4"
+  end
+rescue Errno::ENOENT
+  false
+end
--- spec/thunderbuddies_spec.rb
+++ spec/thunderbuddies_spec.rb
@@ -5,7 +5,14 @@ describe Thunderbuddies do
     expect(Thunderbuddies::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'returns a valid and reachable url of the thunder alert' do
+    tb = Thunderbuddies::Image.new("Asgald", "/tmp")
+    expect(tb.thunder_url).not_to be false
+  end
+  
+  it 'saves an image to the specified directory' do
+    tb = Thunderbuddies::Image.new("Jotunheim", "/tmp")
+    tb.get_image(tb.thunder_url)
+    expect(File).to exist(tb.image)
   end
 end
